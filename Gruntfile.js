f362de5a17318d0609fb8a0585370f40c2eb8e50
idleberg <jan@fragnebenan.com>
2018-01-04 19:50:48 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    eslint: {
+      all: ['tasks/*.js']
+    },
+    jsonlint: {
+      src: [ './*.json' ],
+      options: {
+        formatter: 'prose'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-jsonlint');
+
+  grunt.registerTask('default', ['eslint', 'jsonlint']);
+
+};
