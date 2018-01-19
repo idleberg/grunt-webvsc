30c2fc24cbf0c2a5934f093a036ce1f2d1096093
idleberg <jan@fragnebenan.com>
2018-01-19 20:38:00 +0100

    first commit

--- test/test.js
+++ test/test.js
@@ -2,8 +2,26 @@
 
 const grunt = require('grunt');
 
-exports.htmlmin = {
-  convert: function (test) {
+exports.webvsc = {
+  empty: function (test) {
+    test.expect(1);
+
+    let actual = grunt.file.read('./test/.tmp/empty.webvs');
+    let expected = grunt.file.read('./test/expected/empty.webvs');
+
+    test.equal(actual, expected, 'should convert AVS');
+    test.done();
+  },
+  misc: function (test) {
+    test.expect(1);
+
+    let actual = grunt.file.read('./test/.tmp/comment.webvs');
+    let expected = grunt.file.read('./test/expected/comment.webvs');
+
+    test.equal(actual, expected, 'should convert AVS');
+    test.done();
+  },
+  render: function (test) {
     test.expect(1);
 
     let actual = grunt.file.read('./test/.tmp/superscope.webvs');
@@ -12,4 +30,13 @@ exports.htmlmin = {
     test.equal(actual, expected, 'should convert AVS');
     test.done();
   },
+  trans: function (test) {
+    test.expect(1);
+
+    let actual = grunt.file.read('./test/.tmp/invert.webvs');
+    let expected = grunt.file.read('./test/expected/invert.webvs');
+
+    test.equal(actual, expected, 'should convert AVS');
+    test.done();
+  }
 };
